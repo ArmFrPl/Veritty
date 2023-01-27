05eb644da1e1eda35642ccdf3511a5f1ebacb0f3
ArmFrPl <ehakobkekhvyan7@gmail.com>
2023-01-27 16:31:04 +0400

    fixed bugs

---
+++ src/Hooks/getLeaders.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+async function useGetLeaders() {
+
+  const res = await axios.get(`http://localhost:5001/leaderboard`)
+  return res.data;
+}
+
+export default useGetLeaders;
