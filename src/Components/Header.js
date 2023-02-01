0e8a98391299b7901edcc9c8445dbead7cac7cad
ArmFrPl <ehakobkekhvyan7@gmail.com>
2023-02-01 13:20:49 +0400

    minor changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -121,7 +121,7 @@ export const Header = ({isLoggedIn, view, setView, menuOpen, ticketCounts}) => {
               flex: 'none',
               order: 1,
               flexGrow: 0,
-              zIndex: 999,
+              zIndex: 50,
               position: 'absolute',
               bottom: '30%',
               left: '21.5%',
